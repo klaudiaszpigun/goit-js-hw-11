9b6296967bf1447b82037f464445977220250130
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-02-24 15:03:02 +0100

    upd

--- src/api.js
+++ src/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-export const fetchData = nameOfPhotos => {
+export const fetchData = async nameOfPhotos => {
   const key = '42539798-27c3408c7f5dca4caada8a6c7';
-  return axios
-    .get(
-      `https://pixabay.com/api/?key=${key}&q=${nameOfPhotos}&image_type=photo`
-    )
-    .then(response => response.data);
+  const response = await axios.get(
+    `https://pixabay.com/api/?key=${key}&q=${nameOfPhotos}&image_type=photo`
+  );
+  return response.data;
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchData } from './api';
 
 const input = document.querySelector('input');
 const button = document.querySelector('button');
+const container = document.querySelector('.container');
 
 button.addEventListener('click', async event => {
   event.preventDefault();
   try {
     const response = await fetchData(input.value);
     const total = response.total;
+
     if (total !== undefined && total > 0) {
       Notify.success(`Hooray! We found ${total}`);
+      const renderCode = response => {
+        const array = response.hits;
+        const result = array
+          .map(({ previewURL, likes, comments, views, downloads }) => {
+            return `<img src="${previewURL}" alt="${input.value}" width="150" height="84">
+                <div>
+                  <strong>likes:</strong>
+                  <p>${likes}</p>
+                  <strong>views:</strong>
+                  <p>${views}</p>
+                  <strong>comments:</strong>
+                  <p>${comments}</p>
+                  <strong>downloads:</strong>
+                  <p>${downloads}</p>
+                </div>`;
+          })
+          .join('');
+        container.insertAdjacentHTML('afterbegin', result);
+      };
+
+      renderCode(response);
     } else {
       Notify.failure('Try again');
     }
