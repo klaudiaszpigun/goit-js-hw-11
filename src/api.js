b6cbef4754d340ac019a2af21630d5ed4edc83ff
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-02-24 13:47:44 +0100

    upd

--- src/api.js
+++ src/api.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export const fetchPhotos = nameOfPhotos => {
   const key = '42539798-27c3408c7f5dca4caada8a6c7';
-  axios
+  return axios
     .get(
-      `https://pixabay.com/api/?key=${key}&q=${nameOfPhotos}&image_type=photo&pretty=true`
+      `https://pixabay.com/api/?key=${key}&q=${nameOfPhotos}&image_type=photo`
     )
     .then(response => response.data);
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import Notiflix from 'notiflix';
+import { fetchPhotos } from './api';
+
 const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.addEventListener('click', async event => {
+  event.preventDefault();
+  try {
+    const response = await fetchPhotos(input.value);
+    const total = response.total;
+    if (total !== undefined && total > 0) {
+      Notiflix.Notify.success(`Hooray! We found ${total}`);
+    } else {
+      Notiflix.Report.Warning(`Try again`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
