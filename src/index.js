2f83b22290cdcbc8c222c3a7693d0f4fd88704ca
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-02-25 13:22:21 +0100

    upd

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { fetchData } from './api';
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 const container = document.querySelector('.gallery');
+const fetchMore = document.querySelector('.load-more');
 
 button.addEventListener('click', async event => {
   event.preventDefault();
@@ -15,9 +16,24 @@ button.addEventListener('click', async event => {
       Notify.success(`Hooray! We found ${total}`);
       const renderCode = response => {
         const array = response.hits;
-        const result = array
-          .map(({ webformatURL, likes, comments, views, downloads }) => {
-            return `<div class="photo-card">
+        const result = renderPhotos(array);
+        container.insertAdjacentHTML('afterbegin', result);
+        fetchMore.classList.remove('load-more');
+      };
+
+      renderCode(response);
+    } else {
+      Notify.failure('Try again');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const renderPhotos = photos => {
+  array
+    .map(({ webformatURL, likes, comments, views, downloads }) => {
+      return `<div class="photo-card">
                       <img src="${webformatURL}" alt="${input.value}" loading="lazy" />
                         <div class="info">
                           <p class="info-item">
@@ -37,16 +53,31 @@ button.addEventListener('click', async event => {
                             ${downloads}
                           </p>
                         </div>`;
-          })
-          .join('');
-        container.insertAdjacentHTML('afterbegin', result);
-      };
+    })
+    .join('');
+};
 
-      renderCode(response);
-    } else {
-      Notify.failure('Try again');
+let page = 1;
+let perpage = 40;
+
+fetchMore.addEventListener('click', async () => {
+  try {
+    const fetchMorePhotos = await fetchMoreCallback(input.value);
+    renderPhotos(fetchMorePhotos);
+    page += 1;
+    if (page > 1) {
+      fetchMore.textContent = 'fetch more photos';
     }
   } catch (error) {
     console.log(error);
   }
 });
+
+const fetchMoreCallback = () => {
+  const params = new URLSearchParams({
+    _limit: perPage,
+    _page: page,
+  });
+
+  fetchDataa(input.value);
+};
