0328cc781d1a0ebc2ac52dc988fcac79e561654e
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-02-25 19:13:48 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const fetchMore = document.querySelector('.load-more');
 const fetchData = async nameOfPhotos => {
   const key = '42539798-27c3408c7f5dca4caada8a6c7';
   const response = await axios.get(
-    `https://pixabay.com/api/?key=${key}&q=${nameOfPhotos}&image_type=photo&_limit=40&page=1`
+    `https://pixabay.com/api/?key=${key}&q=${nameOfPhotos}&image_type=photo&_limit=40&page=19`
   );
   return response.data;
 };
